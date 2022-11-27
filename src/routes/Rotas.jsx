8322a0b953f796a077114fffcfa1bffecc080175
Fabiano <Fabibinho2@gmail.com>
2022-11-27 05:28:37 -0300

    Alterando o json do projeto

--- src/routes/Rotas.jsx
+++ src/routes/Rotas.jsx
@@ -1,30 +1,18 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, HashRouter } from 'react-router-dom'
 import Home from '../components/Home'
 import ExplorarJogos from '../components/ExplorarJogos'
 import JogosPage from '../components/Jogos'
 import PromocoesPage from '../components/PromocoesPage'
-import { useState } from 'react'
-import { useEffect } from 'react'
-
 
 const Rotas = () => {
- 
-     const [jogos, setJogos] = useState({nome: "Resident Evil 7: Biohazard"})
-     
-
-     
     return(
         <div>
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home/>}/>
                 <Route path="/explorar-jogos" element={<ExplorarJogos/>} />
-                <Route path="/jogos-page/:nome"  
-                element={<JogosPage jogos={jogos}/>}/>
-                <Route path="/Promocoes"  
-                element={<PromocoesPage/>}/>
-                    
-                
+                <Route path="/jogos-page/:nome" element={<JogosPage />}/>
+                <Route path="/promocoes" element={<PromocoesPage/>}/>
             </Routes>
         </BrowserRouter>
         </div>
